2217c7d1791b077ed5fffecd11f32c357d052d3a
Chamara Paul <chamara.paul@gmail.com>
2013-06-03 17:37:11 -0400

    created ios project

---
+++ CivicsMaster/CivicsMaster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CivicsMaster
+//
+//  Created by Chamara Paul on 6/3/13.
+//  Copyright (c) 2013 CitizenshipFirst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
